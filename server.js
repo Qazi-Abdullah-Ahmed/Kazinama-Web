15893ece908c64ae09954a2849077a18c3a95a33
Qazi-Abdullah-Ahmed <qaziabdullahahmed@gmail.com>
2025-06-09 21:41:47 +0530

    Font Fix Try 2

--- server.js
+++ server.js
@@ -1,9 +1,20 @@
 const express = require("express");
+const path = require("path");
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
-// Serve static files from the 'public' directory
-app.use(express.static("public"));
+// Serve static files with proper font MIME types
+app.use(express.static(path.join(__dirname, "public"), {
+    setHeaders: (res, filePath) => {
+        if (filePath.endsWith(".woff2")) {
+            res.setHeader("Content-Type", "font/woff2");
+        } else if (filePath.endsWith(".woff")) {
+            res.setHeader("Content-Type", "font/woff");
+        } else if (filePath.endsWith(".ttf")) {
+            res.setHeader("Content-Type", "font/ttf");
+        }
+    }
+}));
 
 app.listen(port, () => {
     console.log(`Server running at http://localhost:${port}`);
